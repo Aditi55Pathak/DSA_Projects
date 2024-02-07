fbe93fb884ef06a7c0083f5fc12ad334db3706f9
aditi55pathak@gmail.com <aditipathakp@gmailcom>
2024-02-07 19:32:26 +0530

    Functionality added

--- Array/Theater.py
+++ Array/Theater.py
@@ -1,2 +1,37 @@
 # Creating gradebook for students
 
+gradebook = [
+    ["Aditi", 91, 98, 90],
+    ["Avani", 12, 45, 22],
+    ["Puja", 56, 66, 89],
+    ["Ishita", 99, 90, 78],
+    ["Heli", 56, 89, 21],
+    ["Pranjal", 89, 90, 12],
+]
+
+
+def printGradeBook(gradebook):
+    for i in gradebook:
+        print(i)
+
+
+def calculateAverage(studentName):
+    for i in gradebook:
+        if i[0] == studentName:
+            grades = i[1:]
+            average = sum(grades) / len(grades)
+            print(average)
+    return -1
+
+
+print("----- Gradebook for Pdeu Students -----")
+printGradeBook(gradebook)
+
+print("Enter the student name: ")
+name = input()
+avg = calculateAverage(name)
+
+if avg != -1:
+    print(f"The average grade of {name} is {avg}")
+else:
+    print(f"Record not found!")
