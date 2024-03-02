c0e6d4bc4a5a9adf3f13f4b412f8f135dbd68ddd
Aditi Pathak <80877301+Aditi55Pathak@users.noreply.github.com>
2024-03-02 19:57:41 +0530

    Update linearQueue.py

--- QueueHere/linearQueue.py
+++ QueueHere/linearQueue.py
@@ -3,13 +3,14 @@
 import time
 import random
 
+
 class PrinterClass:
     def __init__(self):
-        self.queue=[]
-    
-    def addLocalJobs(self,jobs):
+        self.queue = []
+
+    def addLocalJobs(self, jobs):
         self.queue.append(jobs)
-    
+
     def process_queue(self):
         while self.queue:
             job = self.queue.pop(0)
@@ -20,3 +21,17 @@ class PrinterClass:
 
             print(f"Job {job} completed")
 
+
+def main():
+    printer_queue = PrinterClass()
+
+    # Add some jobs to the queue
+    for i in range(1, 6):
+        printer_queue.addLocalJobs(f"Job{i}")
+
+    # Process the queue
+    printer_queue.process_queue()
+
+
+if __name__ == "__main__":
+    main()
