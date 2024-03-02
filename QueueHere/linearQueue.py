4044e3bb215ae33b48321054744f8b863dd6a72f
aditi55pathak@gmail.com <aditipathakp@gmailcom>
2024-03-02 20:22:11 +0530

    Merge remote changes

--- QueueHere/linearQueue.py
+++ QueueHere/linearQueue.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Printer queue simulation in Python
 
 import time
@@ -36,42 +35,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-# Printer queue simulation in Python
-
-import time
-import random
-
-
-class PrinterClass:
-    def __init__(self):
-        self.queue = []
-
-    def addLocalJobs(self, jobs):
-        self.queue.append(jobs)
-
-    def process_queue(self):
-        while self.queue:
-            job = self.queue.pop(0)
-            print(f"Printing job {job}")
-
-            # Simulate printing time
-            time.sleep(random.uniform(0.5, 1.5))
-
-            print(f"Job {job} completed")
-
-
-def main():
-    printer_queue = PrinterClass()
-
-    # Add some jobs to the queue
-    for i in range(1, 6):
-        printer_queue.addLocalJobs(f"Job{i}")
-
-    # Process the queue
-    printer_queue.process_queue()
-
-
-if __name__ == "__main__":
-    main()
->>>>>>> c0e6d4bc4a5a9adf3f13f4b412f8f135dbd68ddd
