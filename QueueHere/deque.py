6bcd0b83ff51cd7c1816922cc6b942b4d3e3695a
Aditi Pathak <Aditi55Pathak>
2024-03-03 23:08:46 +0530

    dequeue commited

--- QueueHere/deque.py
+++ QueueHere/deque.py
@@ -1,2 +1,22 @@
-class Ola:
-    pass
+# double ended queue for a book shelf where you can insert or remove the books from both ends
+
+from collections import deque
+
+class Bookshelf:
+    def __init__(self):
+        self.bookshelf=deque()
+    
+    def appendbookleft(self,book):
+        self.bookshelf.appendleft(book)
+
+    def appendbookright(self,book):
+        self.bookshelf.append(book)
+
+    def removebookleft(self):
+        if len(self.bookshelf)>0:
+            self.bookshelf.popleft()
+        else:
+            return None
+        
+    def removebookright(self):
+        pass
