f0c4af781b3303dda1b382c627c6a7f8d124f598
Aditi Pathak <Aditi55Pathak>
2024-03-04 20:15:13 +0530

    Comitted

---
+++ QueueHere/Circular.py
@@ -0,0 +1,10 @@
+# Rotating Sushi Bowl System at resturent
+
+class CircularQueue:
+    def __init__(self,capacity):
+        self.capacity=capacity
+        self.queue=[None]*capacity
+        
+    
+        
+        
